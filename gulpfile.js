ce627b1fca0749fb06d3bcc53cebdc99f38d1000
José Luis Acosta Zamora <jluis.acosta@outlook.com>
2016-11-25 14:17:59 -0600

    automation tasks

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var smoosh = require('gulp-smoosher');
+
+var scssFiles = [ 'grid', 'style', 'rd-mailform', 'google-map', 'mailform', 'contact-form' ];
+
+scssFiles = scssFiles.map(function(el){ return './src/sass/' + el + '.scss'; });
+
+var config = {
+  view: {
+    src: './src/index.html',
+    dest: './'
+  },
+  styles: {
+    src: scssFiles,
+    dest: './css'
+  }
+};
+
+gulp.task('build:html', () => {
+  gulp.src(config.view.src)
+    .pipe(smoosh())
+    .pipe(gulp.dest(config.view.dest));
+});
+
+gulp.task('build:css', () => {
+  gulp.src(config.styles.src)
+    .pipe(sass({
+      errLogToConsole: true,
+      outputStyle: 'expanded'//'compressed' || 'expanded'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(config.styles.dest));
+});
+
+gulp.task('default', ['build:css', 'build:html']);
